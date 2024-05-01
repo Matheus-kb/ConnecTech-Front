c9a73db901bcd51b842c90de53d6f293fe0b5fe9
Matheus-kb <matheuskbitencourt@gmail.com>
2024-05-01 14:55:14 -0300

    feat: adding menu burger on header

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,32 @@
-"use client"
-import { ChevronLeft } from "lucide-react";
+"use client";
+import { ChevronLeft, Menu } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { Card, CardContent } from "./ui/card";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
+import SideMenu from "./side-menu";
 
 const Header = () => {
-    const router = useRouter();
-    const goBack = () => router.back();
+  const router = useRouter();
+  const goBack = () => router.back();
 
-    return ( 
-        <div className="flex bg-primary-foreground">
-            <ChevronLeft onClick={goBack} className="min-w-9 min-h-9 m-2"/>
-        </div>
-     );
-}
- 
+  return (
+    <Card className="max-h-12">
+      <CardContent className="flex flex-row justify-between items-center pt-1">
+        <ChevronLeft onClick={goBack} className="min-w-9 min-h-9" />
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="ghost">
+              <Menu className="min-w-9 min-h-9" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+                <SideMenu />
+          </SheetContent>
+        </Sheet>
+      </CardContent>
+    </Card>
+  );
+};
+
 export default Header;
