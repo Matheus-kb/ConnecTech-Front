715f1451ae04fcb8b5ec03c1383864ed25219c00
Matheus-kb <matheuskbitencourt@gmail.com>
2024-05-01 16:42:45 -0300

    feat: edit profile page

---
+++ app/profile/edit/page.tsx
@@ -0,0 +1,202 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { AtSign, LockKeyholeIcon, User } from "lucide-react";
+import Header from "@/components/header";
+import Image from "next/image";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "O nome de usuário deve ter pelo menos 2 caracteres",
+  }),
+  email: z
+    .string()
+    .email({ message: "Por favor, insira um endereço de e-mail válido" }),
+  password: z
+    .string()
+    .min(6, { message: "A senha deve ter pelo menos 6 caracteres" }),
+  repeatpassword: z
+    .string()
+    .min(6, { message: "A senha deve ter pelo menos 6 caracteres" })
+    .max(10, { message: "A senha deve ter no máximo 10 caracteres" }),
+});
+
+const EditProfilePage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      repeatpassword: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center h-[90vh]">
+        <div className="flex flex-row gap-4  pb-12">
+          <div className="relative w-12 h-12">
+            <Image
+              src="/profile.png"
+              alt="Foto de perfil"
+              className="rounded-full"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <h1 className="uppercase font-bold text-xl pt-2">
+            Faça seu cadastro
+          </h1>
+        </div>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-bold">Nome do usuário</FormLabel>
+                    <div className="flex flex-row gap-2">
+                      <User className="mt-2" />
+                      <FormControl>
+                        <Input
+                          className="min-w-72"
+                          placeholder="Digite o nome"
+                          {...field}
+                        />
+                      </FormControl>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-bold">Email</FormLabel>
+                    <div className="flex flex-row gap-2">
+                      <AtSign className="mt-2" />
+                      <FormControl>
+                        <Input
+                          className="min-w-72"
+                          placeholder="Digite um email válido"
+                          {...field}
+                        />
+                      </FormControl>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-bold">Senha</FormLabel>
+                    <div className="flex flex-row gap-2">
+                      <LockKeyholeIcon className="mt-2" />
+                      <FormControl>
+                        <Input
+                          className="min-w-72"
+                          placeholder="Digite uma senha de 6 á 10 caracteres"
+                          {...field}
+                        />
+                      </FormControl>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="repeatpassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-bold">Confirmar senha</FormLabel>
+                    <div className="flex flex-row gap-2">
+                      <LockKeyholeIcon className="mt-2" />
+                      <FormControl>
+                        <Input
+                          className="min-w-72"
+                          placeholder="Digite sua senha novamente"
+                          {...field}
+                        />
+                      </FormControl>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div>
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button
+                      className="min-w-[18.75rem] rounded-3xl font-bold text-xl uppercase"
+                      type="submit"
+                    >
+                      Confirmar
+                    </Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent className="max-w-72 rounded-lg">
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Confirmar as alterações?
+                      </AlertDialogTitle>
+                    </AlertDialogHeader>
+                    <div className="flex flex-row justify-center items-center gap-12">
+                      <AlertDialogAction className="rounded-full">SIM</AlertDialogAction>
+                      <AlertDialogCancel className="mt-0 rounded-full">NÃO</AlertDialogCancel>
+                    </div>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </div>
+              <div>
+                <Button
+                  className="min-w-[18.75rem] rounded-3xl font-bold text-xl uppercase"
+                  variant="outline"
+                >
+                  Cancelar
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditProfilePage;
