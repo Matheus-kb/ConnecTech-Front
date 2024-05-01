d8b36fe1c18da6babec837cb5e3ee4f87a1fb037
Matheus-kb <matheuskbitencourt@gmail.com>
2024-05-01 18:07:24 -0300

    feat: event informations page

---
+++ app/events/[id]/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/header";
+import EventInfo from "./components/event-info";
+
+const EventDetailsPage = () => {
+    return ( <>
+    <Header />
+    <EventInfo />
+    </> );
+}
+ 
+export default EventDetailsPage;
