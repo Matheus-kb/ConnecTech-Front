7b7167c9c3c507dc8b7d1655b2aef37de81bb8ad
Matheus-kb <matheuskbitencourt@gmail.com>
2024-07-09 20:13:54 -0300

    feat: desktop responsiveness to initial page

--- app/initial/page.tsx
+++ app/initial/page.tsx
@@ -1,20 +1,39 @@
+"use client";
+import Header2 from "@/components/header2";
 import { Button } from "@/components/ui/button";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 const InitialPage = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleLogin = () => { router.push('/login') }
-    const handleRegister = () => { router.push('/register') }
-    return ( 
-        <div className="flex justify-center items-center h-screen">
-            <div className="flex flex-col gap-1 px-4 mt-80">
-            <Button className="rounded-3xl font-bold min-w-[18.75rem] min-h-11" onClick={handleLogin}>Login</Button>
-            <p className="text-center font-bold">ou</p>
-            <Button className="rounded-3xl font-bold min-w-[18.75rem] min-h-11" onClick={handleRegister}>Cadastre-se</Button>
+  const handleLogin = () => {
+    router.push("/login");
+  };
+  const handleRegister = () => {
+    router.push("/register");
+  };
+  return (
+    <>
+        <Header2 />
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col gap-1 px-4 mt-80 lg:mt-44">
+          <Button
+            className="rounded-3xl font-bold min-w-[18.75rem] min-h-11 lg:text-base"
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+          <p className="text-center font-bold">ou</p>
+          <Button
+            className="rounded-3xl font-bold min-w-[18.75rem] min-h-11 lg:text-base"
+            onClick={handleRegister}
+          >
+            Cadastre-se
+          </Button>
         </div>
-        </div>
-     );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default InitialPage;
