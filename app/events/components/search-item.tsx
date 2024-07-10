abe16016bb0d650c5146477442b8f965f0a51f97
Matheus-kb <matheuskbitencourt@gmail.com>
2024-07-09 22:16:21 -0300

    feat: display of all events responsively

--- app/events/components/search-item.tsx
+++ app/events/components/search-item.tsx
@@ -1,12 +1,26 @@
+import { EventType } from "@/app/_types/event";
 import { Card } from "@/components/ui/card";
+import { format } from "date-fns";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-const SearchItem = () => {
+interface EventItemProps {
+  event: EventType;
+}
+
+
+
+const SearchItem: React.FC<EventItemProps> = ({ event }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/events/${event.id}`);
+  };
   return (
-    <div className="min-w-full max-w-full px-7">
-      <Card>
+    <div onClick={handleClick} className="min-w-full max-w-full px-7 lg:max-w-72 lg:min-w-72 lg:py-8 lg:px-0">
+      <Card className=" lg:max-w-72 lg:max-h-60">
         <div className="py-0">
-          <div className="w-full h-[5rem] relative">
+          <div className="w-full h-[5rem] relative lg:w-72 lg:h-60">
             <Image
               src="/image.png"
               alt="Banner do evento"
@@ -16,10 +30,10 @@ const SearchItem = () => {
             />
           </div>
           <h1 className="font-bold text-2xl overflow-hidden text-ellipsis text-nowrap">
-            Nome do evento kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+            {event.title}
           </h1>
-          <p className="font-semibold overflow-hidden text-ellipsis text-nowrap">
-            Dia ° Data ° Horario
+          <p className="font-semibold">
+          {format(new Date(event.date), 'dd/MM/yyyy HH:mm')}
           </p>
         </div>
       </Card>
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,19 +1,32 @@
+"use client"
+
 import Header from "@/components/header";
 import Search from "../(home)/components/search";
 import SearchItem from "./components/search-item";
+import { useEffect, useState } from "react";
+import { EventType } from "../_types/event";
+import api from "../_api/api";
 
 const EventsPage = () => {
+    const [events, setEvents] = useState<EventType[]>([]);
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+          const response = await api.get("/events");
+          setEvents(response.data);
+        };
+        fetchEvents();
+    }, []);
     return ( 
         <>
         <Header />
         <div className="py-5">
             <Search />
         </div>
-        <div className="flex flex-col gap-4">
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
+        <div className="flex flex-col gap-4 lg:flex-row lg:flex-wrap lg:justify-center">
+        {events.map((event) => (
+            <SearchItem key={event.id} event={event} />
+          ))}
         </div>
         </>
      );
