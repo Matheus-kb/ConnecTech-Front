c91da39fd955cec053c4703c202fe21bf1079013
Jose Prestes Neto <980185@queroquero.local>
2024-07-17 19:19:31 -0300

    feat: integracao api pgto

--- app/events/[id]/components/event-info.tsx
+++ app/events/[id]/components/event-info.tsx
@@ -19,7 +19,10 @@ const EventInfo: React.FC<EventInfoProps> = ({ event }) => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [imageUrl, setImageUrl] = useState<string>("");
   const [volunteers, setVolunteers] = useState<Volunteer[]>([]);
-  const data = sessionStorage.getItem("user") === null ? null : JSON.parse(sessionStorage.getItem("user") || "");
+  const data =
+    sessionStorage.getItem("user") === null
+      ? null
+      : JSON.parse(sessionStorage.getItem("user") || "");
 
   useEffect(() => {
     getImageUrl();
@@ -28,7 +31,9 @@ const EventInfo: React.FC<EventInfoProps> = ({ event }) => {
 
   const getImageUrl = async () => {
     try {
-      const response = await fetch(`http://localhost:3000/upload/event/${event.id}/banner`);
+      const response = await fetch(
+        `http://localhost:3000/upload/event/${event.id}/banner`
+      );
 
       if (!response.ok) {
         throw new Error(`Erro na requisição: ${response.statusText}`);
@@ -38,7 +43,7 @@ const EventInfo: React.FC<EventInfoProps> = ({ event }) => {
       const imageUrl = `${imageName}`;
       setImageUrl(imageUrl);
     } catch (error) {
-      console.error('Erro ao buscar a imagem:', error);
+      console.error("Erro ao buscar a imagem:", error);
     }
   };
 
@@ -53,7 +58,7 @@ const EventInfo: React.FC<EventInfoProps> = ({ event }) => {
 
   const handleBuyTicket = async () => {
     try {
-      const response = await api.post("/payment", {
+      const response = await api.post(`events/${event.id}/payment`, {
         userId: 1,
         eventId: event.id,
         value: 1000,
@@ -61,9 +66,48 @@ const EventInfo: React.FC<EventInfoProps> = ({ event }) => {
         eventTitle: event.title,
         image: "https://via.placeholder.com/150",
       });
+
       const confirma = window.confirm("Confirme sua compra de ingresso");
       if (confirma) {
-        window.alert("Compra realizada com sucesso!");
+        window.open(response.data.link);
+
+        const interval = 2000; // Intervalo de 2 segundos
+        const duration = 5 * 60 * 1000; // 5 minutos em milissegundos
+        const endTime = Date.now() + duration;
+
+        const options = {
+          method: "GET",
+          headers: {
+            accept: "*/*",
+            Authorization:
+              "Bearer 5aad7227-9f7d-4093-83fa-90d9ffe0fba135dd4d9f42d1adaf5fcc75648df24696d019-ed41-4ec3-a783-ef2c34849a46",
+          },
+        };
+
+        const checkPaymentStatus = async () => {
+          try {
+            const statusResponse = await axios.get(
+              `https://sandbox.api.pagseguro.com/checkouts/${response.data.id}`,
+              options
+            );
+            console.log("Status do pagamento:", statusResponse);
+
+            if (statusResponse.data === "paid") {
+              clearInterval(polling);
+              window.alert("Pagamento confirmado com sucesso!");
+              // Faça qualquer ação adicional necessária após confirmação do pagamento
+            } else if (Date.now() >= endTime) {
+              clearInterval(polling);
+              window.alert(
+                "Tempo excedido. Verifique o status do pagamento mais tarde."
+              );
+            }
+          } catch (error) {
+            console.error("Erro ao verificar status do pagamento:", error);
+          }
+        };
+
+        const polling = setInterval(checkPaymentStatus, interval);
       }
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -75,7 +119,9 @@ const EventInfo: React.FC<EventInfoProps> = ({ event }) => {
   const handleVolunteerAssociation = async () => {
     try {
       const userId = data.id; // Substitua com a lógica para obter o ID do usuário logado
-      const response = await api.post(`/volunteers/${userId}/associate/${event.id}`);
+      const response = await api.post(
+        `/volunteers/${userId}/associate/${event.id}`
+      );
       toast.success("Você se associou com sucesso ao evento!");
       fetchVolunteers(); // Atualize a lista de voluntários após a associação
     } catch (error) {
@@ -119,7 +165,7 @@ const EventInfo: React.FC<EventInfoProps> = ({ event }) => {
     <div className="flex flex-col justify-center lg:items-center pb-5">
       <div className="relative w-full h-44 lg:h-96 lg:w-[80%] lg:mx-auto lg:my-8">
         <Image
-          src={imageUrl || '/image.png'}
+          src={imageUrl || "/image.png"}
           alt="Banner do evento"
           fill
           style={{ objectFit: "cover" }}
@@ -154,22 +200,31 @@ const EventInfo: React.FC<EventInfoProps> = ({ event }) => {
             <table className="min-w-full bg-white border border-gray-200">
               <thead>
                 <tr>
-                  <th className="px-4 py-2 border-b-2 border-gray-200 text-left">ID</th>
-                  <th className="px-4 py-2 border-b-2 border-gray-200 text-left">Nome</th>
+                  <th className="px-4 py-2 border-b-2 border-gray-200 text-left">
+                    ID
+                  </th>
+                  <th className="px-4 py-2 border-b-2 border-gray-200 text-left">
+                    Nome
+                  </th>
                 </tr>
               </thead>
               <tbody>
                 {volunteers.map((volunteer, index) => (
                   <tr key={volunteer.id}>
-                    <td className="px-4 py-2 border-b border-gray-200">{index + 1}</td>
-                    <td className="px-4 py-2 border-b border-gray-200">{volunteer.name}</td>
+                    <td className="px-4 py-2 border-b border-gray-200">
+                      {index + 1}
+                    </td>
+                    <td className="px-4 py-2 border-b border-gray-200">
+                      {volunteer.name}
+                    </td>
                   </tr>
-
                 ))}
               </tbody>
             </table>
           ) : (
-            <p className="text-red-500">Nenhum voluntário associado a este evento.</p>
+            <p className="text-red-500">
+              Nenhum voluntário associado a este evento.
+            </p>
           )}
         </div>
       </div>
@@ -188,10 +243,12 @@ const EventInfo: React.FC<EventInfoProps> = ({ event }) => {
         </Button>
       </div>
       <div className="flex flex-col items-center mt-4">
-        <input type="file" className="text-sm mb-2" onChange={handleFileChange} />
-        <Button onClick={uploadImage}>
-          Upload Imagem
-        </Button>
+        <input
+          type="file"
+          className="text-sm mb-2"
+          onChange={handleFileChange}
+        />
+        <Button onClick={uploadImage}>Upload Imagem</Button>
       </div>
     </div>
   );
